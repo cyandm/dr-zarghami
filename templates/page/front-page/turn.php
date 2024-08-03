2903a40ea0ce7a0a067da16c5b9beae09b336af5
amirali-dizabadi <amirali.dizabadi@gmail.com>
2024-08-03 12:32:42 +0330

    v 0.1.2

--- templates/page/front-page/turn.php
+++ templates/page/front-page/turn.php
@@ -1,12 +1,12 @@
 <?php
 
 ?>
-<section class="container">
+<section class="container" id="contact">
     <div class="title">
         <h2><?= get_field('turn_title'); ?></h2>
         <p><?= get_field('turn_subtitle'); ?></p>
     </div>
- <?=   get_template_part('templates/components/contact',null,);  ?>
+    <?= get_template_part('templates/components/contact', null,);  ?>
 
 
 
